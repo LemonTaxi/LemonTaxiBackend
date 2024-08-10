6a3fe8eb97306cd42956771ffa5982946c37fd05
Ian <ian@lemonbase.com>
2024-08-11 03:16:17 +0900

    fix:cors middleware

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 import dotenv
 from fastapi import FastAPI
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 from ui.router import init_router
@@ -9,23 +10,25 @@ dotenv.load_dotenv(dotenv_file)
 
 
 def create_app() -> FastAPI:
+    origins = ["*"]
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    ]
+
     app = FastAPI(
         debug=True,
+        middleware=middleware,
         title="Lemonbase Taxi",
         version="0.0.1",
     )
 
     init_router(app)
-
-    origins = ["*"]
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
 
     return app
 
