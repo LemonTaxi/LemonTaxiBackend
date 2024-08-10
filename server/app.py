417f4fd14f386d12a367d4b9c6efea689934073c
Ian <ian@lemonbase.com>
2024-08-11 03:11:48 +0900

    fix:cors middleware

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 import dotenv
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from ui.router import init_router
 
@@ -22,7 +22,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
-        allow_credentials=False,
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
